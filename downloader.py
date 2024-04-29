991582b8905db551fa8d8667d44cc054801de2ee
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-29 11:02:12 +0600

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,29 +1,25 @@
 import asyncio
 import os
 import config
+import requests
 
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.types import InputFile
-import aiohttp
-import youtube_dl
-from youtube_dl.utils import DownloadError
 
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
 
 async def download_video(url: str) -> str:
-    ydl_opts = {
-        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4',
-        'outtmpl': 'downloaded_video.mp4'
-    }
-    try:
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-    except DownloadError:
+    response = requests.get(url)
+    if response.status_code == 200:
+        filename = 'downloaded_video.mp4'
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+        return filename
+    else:
         return None
-    return 'downloaded_video.mp4'
 
 
 @dp.message_handler(commands=['start'])
@@ -40,7 +36,7 @@ async def process_video(message: types.Message):
             await message.reply_document(InputFile(file_path))
             os.remove(file_path)
         else:
-            await message.reply("Failed to download the video. The requested format may not be available.")
+            await message.reply("Failed to download the video. Please check the URL and try again.")
     else:
         await message.reply("Unsupported file format. Please provide a link to a video in MKV or MP4 format.")
 
