912fd3234fd0c9ea14264689c15737bce97e6afb
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-29 10:44:53 +0600

    Create downloader.py

---
+++ downloader.py
@@ -0,0 +1,47 @@
+import asyncio
+import os
+import config
+
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.types import InputFile
+import aiohttp
+import youtube_dl
+
+bot = Bot(token=config.TOKEN)
+dp = Dispatcher(bot)
+
+
+async def download_video(url: str) -> str:
+    ydl_opts = {
+        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4',
+        'outtmpl': 'downloaded_video.mp4'
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    return 'downloaded_video.mp4'
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.reply("Send me the link to the video you want to download.")
+
+
+@dp.message_handler()
+async def process_video(message: types.Message):
+    url = message.text
+    if url.endswith('.mkv') or url.endswith('.mp4'):
+        file_path = await download_video(url)
+        if file_path:
+            await message.reply_document(InputFile(file_path))
+            os.remove(file_path)
+        else:
+            await message.reply("Failed to download the video.")
+    else:
+        await message.reply("Unsupported file format. Please provide a link to a video in MKV or MP4 format.")
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.create_task(dp.start_polling())
+    loop.run_forever()
